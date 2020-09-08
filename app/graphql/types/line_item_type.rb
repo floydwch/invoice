08dd8ad1710e96eea46a905a41854de8fb034190
floydwch <floydwch@gmail.com>
2020-09-08 17:33:01 +0800

    change line_item's id type from ID to Int in graphql

--- app/graphql/types/line_item_type.rb
+++ app/graphql/types/line_item_type.rb
@@ -1,6 +1,6 @@
 module Types
   class LineItemType < Types::BaseObject
-    field :id, ID, null: false
+    field :id, Int, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :name, String, null: true
