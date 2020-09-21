38b25e94ec03a3e5b14eeb3ca98aa18a1170e650
floydwch <floydwch@gmail.com>
2020-09-21 21:53:38 +0800

    set graphql-let for automatical type generation

---
+++ web/next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.graphql$/,
+      exclude: /node_modules/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: 'graphql-let/loader',
+        },
+      ],
+    })
+
+    return config
+  },
+}
